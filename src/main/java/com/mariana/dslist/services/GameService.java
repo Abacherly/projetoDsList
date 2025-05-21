e3ba8e2ed1c55c914ffd977f155ab49217340859
Mariana Abacherly <mariana.abacherly@gmail.com>
2025-05-21 18:40:08 -0300

    Game repository, service, controller

---
+++ src/main/java/com/mariana/dslist/services/GameService.java
@@ -0,0 +1,24 @@
+package com.mariana.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariana.dslist.dto.GameMinDTO;
+import com.mariana.dslist.entities.Game;
+import com.mariana.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+}
+ 
