5cb2e8664a3155acb2a9c6e75cb7c64914ca05e5
Mariana Abacherly <mariana.abacherly@gmail.com>
2025-05-23 20:26:26 -0300

    Deploy ready

--- src/main/java/com/mariana/dslist/services/GameService.java
+++ src/main/java/com/mariana/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mariana.dslist.dto.GameDTO;
 import com.mariana.dslist.dto.GameMinDTO;
 import com.mariana.dslist.entities.Game;
+import com.mariana.dslist.projections.GameMinProjection;
 import com.mariana.dslist.repositories.GameRepository;
 
 
@@ -27,6 +28,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
